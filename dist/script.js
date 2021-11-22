ea3aed0bf9213511e9c6d5a506ce6d6bbb53d2c1
Bojan J <bjosilo@gmail.com>
2021-11-22 15:20:00 -0500

    TS introduction and setup

---
+++ dist/script.js
@@ -0,0 +1,5 @@
+var a = '1';
+console.log('aaa', a); // Property foo does not exist on type 1
+// tsc -w // make watcher look for file change and transpile to native JS code
+// it looks for destination in tsconfig.json, if not found we must type relative path
+// tsc ./src/script.ts -w   ...in terminal
---
+++ src/script.ts
@@ -0,0 +1,6 @@
+const a = '1';
+console.log('aaa',a) // Property foo does not exist on type 1
+
+// tsc -w // make watcher look for file change and transpile to native JS code
+// it looks for destination in tsconfig.json, if not found we must type relative path
+// tsc ./src/script.ts in terminal
